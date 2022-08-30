bf0ff39b0c8bf28938a1ef98886f3896fe9a378f
Arne Rief <arne.rief@gmail.com>
2022-08-30 21:21:27 +0200

    filter in bookshelf

--- frontend/src/components/Bookshelf.js
+++ frontend/src/components/Bookshelf.js
@@ -2,11 +2,14 @@ import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MyContext } from "../context/ContextProvider";
 import SearchField from "./SearchField";
+import FilterDisplay from "./FilterDisplay";
 
 const Bookshelf = () => {
   const context = useContext(MyContext);
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState("");
+  const [searchFilter, setSearchFilter] = useState("");
+  const filterBooks = ["all", "author", "title", "category"];
 
   return (
     <section className="bookshelf">
@@ -14,10 +17,11 @@ const Bookshelf = () => {
         action={(event) => setSearchValue(event.target.value)}
         value={searchValue}
       />
+      <FilterDisplay criteria={filterBooks} />
       {context.myBooks.map(
         (book, index) =>
           // filter display according to user search, if any
-          book.includes(searchValue.toLowerCase()) && (
+          book.title.includes(searchValue.toLowerCase()) && (
             <div className="book" key={index}>
               <p className="book-label">
                 <span style={{ fontWeight: "bold" }}>{book.title}</span> <br />
