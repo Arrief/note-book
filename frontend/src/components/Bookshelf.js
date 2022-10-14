03979188eb8fea5237d325740afc60f7ee62e815
Arne Rief <arne.rief@gmail.com>
2022-10-14 22:27:03 +0200

    search functionality for Bookshelf.js

--- frontend/src/components/Bookshelf.js
+++ frontend/src/components/Bookshelf.js
@@ -6,34 +6,37 @@ import SearchField from "./SearchField";
 const Bookshelf = () => {
   const context = useContext(MyContext);
   const navigate = useNavigate();
+
+  // state to keep track of user input in the search field
   const [searchValue, setSearchValue] = useState("");
 
+  const updateSearchValue = (event) => setSearchValue(event.target.value);
+  // function to filter display according to user search input
+  const compareInputToBookTitle = (book) =>
+    book.title.toLowerCase().includes(searchValue.toLowerCase());
+
   return (
-    <section className="bookshelf">
-      <SearchField
-        action={(event) => setSearchValue(event.currentTarget.value)}
-        value={searchValue}
-      />
-      {context.myBooks.map(
-        (book, index) =>
-          // filter display according to user search, if any
-          book.title.includes(searchValue.toLowerCase()) && (
-            <div className="book" key={index}>
-              <p className="book-label">
-                <span style={{ fontWeight: "bold" }}>{book.title}</span> <br />
-                by <br />
-                {book.author}
-              </p>
-              <button
-                className="book-btn"
-                onClick={() => navigate(`/my-books/${book.id}`)}
-              >
-                Details
-              </button>
-            </div>
-          )
-      )}
-    </section>
+    <>
+      <SearchField action={updateSearchValue} value={searchValue} />
+      <section className="bookshelf">
+        {/* display all books by default or apply filter from user search */}
+        {context.myBooks.filter(compareInputToBookTitle).map((book, index) => (
+          <div className="book" key={index}>
+            <p className="book-label">
+              <span style={{ fontWeight: "bold" }}>{book.title}</span> <br />
+              by <br />
+              {book.author}
+            </p>
+            <button
+              className="book-btn"
+              onClick={() => navigate(`/my-books/${book.id}`)}
+            >
+              Details
+            </button>
+          </div>
+        ))}
+      </section>
+    </>
   );
 };
 
